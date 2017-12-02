34f54a2d3d05366d89d86a5f1270c8073ce9c328
Anthony Tracy <gotbordom@gmail.com>
2017-12-02 16:26:50 -0700

    interp2d Seems to work, now to add it to other file

--- sascha_app/interp2d.py
+++ sascha_app/interp2d.py
@@ -6,7 +6,7 @@
 
 def interp2d(data,z,x):
   # Inputs:
-  #  data -	The known [(x1,y1),(x1,y2),(x2,y1),(x1,y1)]
+  #  data -	The known [(x1,y1),(x2,y2)]
   #  z -	The output of f(x,y) for each of the inputs in order of 11,12,21,22....
   #  x -	The (x,y) point to interpolate
   # f(x,y) = a0 + a1x + a2y + a3xy (approximatly)
@@ -23,7 +23,7 @@ def interp2d(data,z,x):
   + (z[1]*pnt2[0]*pnt1[1])/x1x2_y2y1 \
   + (z[2]*pnt1[0]*pnt2[1])/x1x2_y2y1 \
   + (z[3]*pnt1[0]*pnt1[1])/x1x2_y1y2
-
+  
   a1 = (z[0]*pnt2[1])/x1x2_y2y1 \
   + (z[1]*pnt1[1])/x1x2_y1y2 \
   + (z[2]*pnt2[1])/x1x2_y1y2 \
@@ -38,6 +38,22 @@ def interp2d(data,z,x):
   + z[1]/x1x2_y2y1 \
   + z[2]/x1x2_y2y1 \
   + z[3]/x1x2_y1y2
+  
+  print a0,a1,a2,a3,x[0],x[1]
 
-  return a0+a1*x+a2*y+a3*x*y
+  return (a0+a1*x[0]+a2*x[1]+a3*x[0]*x[1])
+
+
+# Testing:
+data = [(2600,52.8),(2685,75.7)]
+z = [888.0,856.0,837,815]	# [f(v1,x1),f(v1,x2),f(v2,x1),f(v2,x2)]
+x = [2650,60]
+
+print data
+print z
+print x
+
+test = interp2d(data,z,x)
+
+print test
 
