3ea7924192cd719838ccee7884ac31892942efca
Anthony Tracy <gotbordom@gmail.com>
2018-03-07 17:47:21 -0700

    just updating my queue for lab3

--- school/Spring2018/csci3753_os/progAssignments/pa3/multi-lookup.c
+++ school/Spring2018/csci3753_os/progAssignments/pa3/multi-lookup.c
@@ -1,113 +1,168 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<pthread.h>
-#include<ipLookup.h>
 
-MODULE_LICENSE("GPL");  // Not sure I reallllly need this for class but hey why not.
-MODULE_AUTHOR("Anthony Tracy");
-MODULE_DESCRIPTION("OS PA3: writing a script to create threads and loopup ip addresses");
-MODULE_VERSION("0.1");
+//MODULE_LICENSE("GPL");  // Not sure I reallllly need this for class but hey why not.
+//MODULE_AUTHOR("Anthony Tracy");
+//MODULE_DESCRIPTION("OS PA3: writing a script to create threads and loopup ip addresses");
+//MODULE_VERSION("0.1");
 
 /* Added the below stuff to a  header.. */
 
-/*
-void *producers(); // What the producer threads will call
-void *consumers(); // What the consumer threads will call
-void wait(struct semaphore *S);
-void signal(struct semaphore *S);
-void init_semaphore(semaphore *S,int n,int buffer);
-void dest_semaphore(semaphore *S)
-*/
+
+//void *producers(void *args); // What the producer threads will call
+//void *consumers(); // What the consumer threads will call
+/* ------------Struct Methods --------------- */
+/* Semaphores */
+
+
+//void wait(struct semaphore *S);
+//void signal(struct semaphore *S);
+//void init_semaphore(semaphore *S,int n,int buffer);
+//void dest_semaphore(semaphore *S)
+/* Queues */
+//queue *queueInit(int size);
+//void queueDelete(queue *q);
+//void enqueue(queue *q, int in);
+//void dequeue(queue *q, int out);
+/* ThreadPools */
+//pool_t *pool_init(void);
+//void pool_del(pool_t *p);
+//void enpool(pool_t *p);
+//void depool(pool_t *p);
 
 
 
-/*--------------------Structs begin---------------------*/
+/* ------------Data structs------------------ */
 
-typedef struct {
-  int value;
-   *list; // who is waiting to be woken 
-}semaphore;
-
-typedef struct {
-  char *fNames;  // files that still need to be read (Make it a bitmap instead?)
-}files2read;
-
-
-
-/*--------------------Semaphore methods begin---------------------*/
-
-// Defining methods for the semiphores .. wish we had classes:
-void wait(semaphore *S){
-  S->value--; // Need memory to store that someone is waiting:
-  // Then check if the current process / thread can take work:
-  if(S->value<0){
-    // blocking process and adding it to memory
-      // Requires appending a process... 
-    block();
-  }//end if
-}//end sig
-
-void signal(semaphore *S){
-  S->value++;
-  // Now if anyone is waiting wake them up:
-  p = S->list[0];
-  if(S->value<=0){
-    // Requires removing a process - not sure how to do this.....
-    wakeup(p);
-  }//end if
-}//end sig
-
-void init_semaphore(semaphore *S,int n,int buffer){
-  S->value=n;
-  // for testing:
-  printf("semaphore has been made with value: %d \n",n);
-  S->list=malloc(sizeof(char)*buffer);
-}//end init
+typedef struct{
+  int max,items;
+  int head, tail;       // Need these locations
+  int full, empty;      // want to know for signaling
+  pthread_mutex_t *mut; // Make sure to have a lock for accessing this queue
+  pthread_cond_t *notFull, *notEmpty; // Make sure to have conditions to signal on
+  char **string;
+} queue;
 
 
-void dest_semaphore(semaphore *S){
-  free(S->list);
+/* Queues */
+queue *queueInit(int size);
+void queueDelete(queue *q);
+void enqueue(queue *q, char *element);
+void dequeue(queue *q);
+char *front(queue *q);
+
+/* ------------Methods----------------------- */
+queue *queueInit(int size){
+  queue *q;
+  q = (queue *)malloc(sizeof(queue));
+
+  q->string = (char**)malloc(sizeof(char*)*size);
+  q->max=size;
+  q->items=0;
+  q->head=0;
+  q->tail=0;
+  q->full=0;
+  q->empty=1;
+  q->mut = (pthread_mutex_t*)malloc(sizeof(pthread_mutex_t));
+  q->notFull = (pthread_cond_t*)malloc(sizeof(pthread_cond_t));
+  pthread_cond_init(q->notFull,NULL); 
+  q->notEmpty = (pthread_cond_t*)malloc(sizeof(pthread_cond_t));
+  pthread_cond_init(q->notEmpty,NULL); 
+
+  return q;
+}
+
+void queueDelete(queue *q){
+  pthread_mutex_destroy (q->mut);
+  free(q->mut);
+  pthread_cond_destroy (q->notFull);
+  free(q->notFull); 
+  pthread_cond_destroy (q->notEmpty);
+  free(q->notEmpty);
+  free(q->string);
+  free(q);
+}
+
+void dequeue(queue *q){
+  /* Make sure queue isn't empty */
+  if(q->items==0){
+    printf("Queue is empty.\n");
+    q->empty=1;
+    q->full=0;
+  }
+  else{
+    q->items--;
+    q->head++;
+    if(q->head==q->max){ // Make it a circular queue
+      q->head=0;
+    }
+  }
+  return;
+}
+
+void enqueue(queue *q,char *element){
+  /* Make sure queue isn't full */
+  if(q->items==q->max){
+    printf("Queue is full.\n");
+    q->empty=0;
+    q->full=1;
+  }
+  else{
+    q->items++;
+    q->tail++;
+    if(q->tail==q->max){
+      q->tail=0;
+    }
+  }
+  return;
+}
+
+char *front(queue *q){
+  /* Make sure that the queue isn't empty */
+  if(q->items==0){
+    printf("Queue is empty.\n");
+    return NULL;
+  }
+  else{
+    printf("Made it this far\n");
+    return (q->string[q->head]); 
+  }
 }
 
 
+/* ------------Main Funciton----------------- */
+int main(){//int argc,char argv[]){
 
-/*---------------------Producer methods begin-------------------------*/
+  char buff[256];
+  //char test[256];
+  FILE *fHandle;
+  size_t nread;
 
-void *producer(){
-  // stuff to do with producer
-  printf("Look I'm a producer!\n");
-  pthread_exit(0);
+  /* Testing my queue, should throw a lot of "Queue is full"...*/
+  queue *q = queueInit(25);
+
+  fHandle=fopen("input/names1.txt","r");
+  if(fHandle) {
+    //while((nread = fread(buff,1,sizeof buff,fHandle)) >0)
+      //fwrite(buff,1,nread,stdout);
+    while(fgets(buff,256,fHandle)!=NULL){
+      printf("Items: %d \n",q->items);
+      printf("%s",buff);
+      if (!(q->full)){
+        enqueue(q,buff);
+      }
+      else{
+        //test=front(q);
+        //fgets(test,256,front(q));
+        printf("Look one from queue:\n");
+        printf("front: %s\n",front(q));
+        dequeue(q);
+        enqueue(q,buff);
+      }
+    }
+  }
+  printf("head: %s\n",q->string[0]);
+  fclose(fHandle);
+  queueDelete(q);
 }
-
-
-
-/*---------------------Consumer methods begin--------------------------*/
-
-
-void *consumer(){
-  // Stuff to do with consumer....
-  printf("Look I'm a consumer!\n");
-  pthread_exit(0);
-}
-  
-/*----------------------Main process begins----------------------------*/
-int main(int argc,char *argv[]){
-  /* Makeing just one producer and one consumer thread for now */
-  pthread_t tid;
-  pthread_attr_t attr; /* use default attrs */
-  
-  /* In here I need to create all my structs */
-
-  /* actually make the producer thread */
-  pthread_attr_init(&attr);
-  pthread_create(&tid,&attr,producer,argv[1]);
-  
-  /* create the consumer*/
-  pthread_create(&tid,&attr,consumer);
-  
-  /* later the above should create a producer & consumer pool */
-  
-  
-  
-}
-
