c8291f0af1b85d26ed23a675670f03a8d652ba29
Anthony Tracy <gotbordom@gmail.com>
2018-03-10 09:22:42 -0700

    Only one block not free per valgrind

--- school/Spring2018/csci3753_os/progAssignments/pa3/multi-lookup.c
+++ school/Spring2018/csci3753_os/progAssignments/pa3/multi-lookup.c
@@ -95,23 +95,23 @@ queue *queueInit(int size){
   q->tail=0;
   q->empty=1;
   q->full=0;
-  //q->mut = (pthread_mutex_t*)malloc(sizeof(pthread_mutex_t));
-  //pthread_mutex_init(q->mut,NULL);
-  //q->notFull = (pthread_cond_t*)malloc(sizeof(pthread_cond_t));
-  //pthread_cond_init(q->notFull,NULL); 
-  //q->notEmpty = (pthread_cond_t*)malloc(sizeof(pthread_cond_t));
-  //pthread_cond_init(q->notEmpty,NULL); 
+  q->mut = (pthread_mutex_t*)malloc(sizeof(pthread_mutex_t));
+  pthread_mutex_init(q->mut,NULL);
+  q->notFull = (pthread_cond_t*)malloc(sizeof(pthread_cond_t));
+  pthread_cond_init(q->notFull,NULL); 
+  q->notEmpty = (pthread_cond_t*)malloc(sizeof(pthread_cond_t));
+  pthread_cond_init(q->notEmpty,NULL); 
 
   return q;
 }
 
 void queueDelete(queue *q){
-  //pthread_mutex_destroy (q->mut);
-  //free(q->mut);
-  //pthread_cond_destroy (q->notFull);
-  //free(q->notFull); 
-  //pthread_cond_destroy (q->notEmpty);
-  //free(q->notEmpty);
+  pthread_mutex_destroy (q->mut);
+  free(q->mut);
+  pthread_cond_destroy (q->notFull);
+  free(q->notFull); 
+  pthread_cond_destroy (q->notEmpty);
+  free(q->notEmpty);
   free(q->string);
   free(q);
 }
